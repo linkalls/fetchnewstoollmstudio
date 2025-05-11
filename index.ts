3bf125faaeb5bd437a48e98bd77c426eaeabfbf2
poteto <naonyan1135@gmail.com>
2025-05-12 00:59:35 +0900

    b

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { LMStudioClient } from "@lmstudio/sdk";
 import { fetchNewsTool } from "./tools";
+import { input } from '@inquirer/prompts';
 
 const client = new LMStudioClient();
 
@@ -9,9 +10,16 @@ const model = await client.llm.model("qwen3-1.7b");
 console.log("モデルの読み込み完了");
 console.log("---------------------")
 
+
+
+const searchQuery = await input({ message: 'なんのニュースについて調べたいですか？' });
+
+console.log(`"${searchQuery}"についてのニュースを取得します。`);
+console.log("---------------------")
+
 // ニュース取得と要約
 await model.act(
-  "最新のHololiveのニュースを要約してください。",
+  `最新の${searchQuery}についてニュースを要約してください。要約には根拠となるurlをつけてください。基本は日本語で検索してください`,
   [fetchNewsTool],
   {
     onMessage: (message) => console.info(message.toString()),
--- tools.ts
+++ tools.ts
@@ -11,14 +11,14 @@ export const fetchNewsTool = tool({
   description: "指定した国と言語の最新ニュースを取得する。",
   parameters: {
     country: z.string().min(2).max(2), // ISO 3166-1形式の国コード（例: 'jp'）
-    language: z.string().min(2).max(2), // ISO 639-1形式の言語コード（例: 'ja'）
+    // language: z.string().min(2).max(2), // ISO 639-1形式の言語コード（例: 'ja'）
     query: z.string().optional(), // 検索キーワード（オプション）
   },
-  implementation: async ({ country, language, query }) => {
+  implementation: async ({ country,  query }) => {
     try {
       const news = await newsService.getNews({
         country, // 国コード
-        language, // 言語コード
+        language: "ja", // 言語コード
         query, // キーワード検索（オプション）
       });
 
@@ -26,8 +26,8 @@ export const fetchNewsTool = tool({
       // 最初の10個のニュースアイテムだけを返す
       return news.items.slice(0, 10).map((item) => ({
         title: item.title,
-        description: item.description,
-        // link: item.link,
+        // description: item.description,
+        link: item.link,
         // pubDate: item.pubDate,
         // source: item.source,
       }));
